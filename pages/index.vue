59190fa44c1be2624e54630d23c64ce3371937c9
Kunal <kunalpaul673@gmail.com>
2024-07-07 02:55:09 +0530

    changed

--- pages/index.vue
+++ pages/index.vue
@@ -16,22 +16,27 @@ import LandingPage from '~/components/LandingPage.vue';
 import Navbar from '~/components/Navbar.vue';
 import products from '../components/products.vue';
 import Buy from '../components/Buy.vue';
-import lucia from '~/server/models/adapter'
+// import lucia from '~/server/models/adapter'
 
+// import { useCookie } from '@nuxt/app';
 
 // const cookie = useCookie('auth_session')
-
+// import {lucia} from 'lucia'
 
 // console.log(cookie)
   export default{
+    components:[LandingPage,Navbar,Buy],
     async setup() {
-      const sessions = await lucia.getUserSessions();
-      console.log(sessions)
+      // const sessions = await lucia.getUserSessions();
+      // console.log(sessions)
+      const cokkie = useCookie('auth_session')
+      // cokkie.value= cokkie.value || 'hhghhh'
+      console.log(cokkie)
+      console.log(cokkie.value)
     const isAuthenticated = true
     return {
       isAuthenticated,
     };
-    components:[LandingPage,Navbar,Buy]
   }
 }
   </script>
